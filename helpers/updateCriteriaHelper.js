475bbc32008bc0995905af9fe5fa115f39452206
sovarantonia <sovarantonia@yahoo.com>
2025-07-10 06:57:08 -0700

    refactored update criteria

--- helpers/updateCriteriaHelper.js
+++ helpers/updateCriteriaHelper.js
@@ -2,17 +2,16 @@ export function updateCriteria({ optionList, option, removingCriteria }) {
   const elementIndex = optionList.findIndex(
     (o) => o.property === option.property,
   );
-  if (removingCriteria(option)) {
-    if (elementIndex !== -1) {
-      return optionList.splice(elementIndex, 1);
-    }
-  } else {
-    if (elementIndex === -1) {
-      optionList.push(option);
-      return optionList;
-    } else {
-      optionList[elementIndex] = option;
-      return optionList;
-    }
+
+  if (elementIndex === -1) {
+    optionList.push(option);
   }
+  
+  if (removingCriteria(option)) {
+    optionList.splice(elementIndex, 1);
+  } else {
+    optionList[elementIndex] = option;
+  }
+
+  return optionList;
 }
